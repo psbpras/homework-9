f5cf94abfc86565f56704f3a4e8c783c83c8c489
Lakshmi Prasanna Pagadala <Lp354@njit.edu>
2025-04-09 15:38:25 +0100

    updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,20 @@
-# conftest.py
+"""Fixtures for QR Code API tests."""
+
 import pytest
 from httpx import AsyncClient
-from app.main import app  # Adjust import path as necessary
+from app.main import app
+
 
 @pytest.fixture
-async def client():
-    async with AsyncClient(app=app, base_url="http://testserver") as ac:
-        yield ac
+async def async_client():
+    """Provides an AsyncClient instance for testing."""
+    async with AsyncClient(app=app, base_url="http://testserver") as client:
+        yield client
+
 
 @pytest.fixture
-async def get_access_token_for_test(client):
+async def get_access_token_for_test(async_client):
+    """Logs in as admin and returns an access token."""
     form_data = {"username": "admin", "password": "secret"}
-    response = await client.post("/token", data=form_data)
+    response = await async_client.post("/token", data=form_data)
     return response.json()["access_token"]
